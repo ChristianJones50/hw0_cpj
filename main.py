f0b0fef89a2ff04d1846092c744bb2d0fff14c11
Christian Jones <cpj5199@psu.edu>
2020-08-28 19:40:36 +0000

    Changed Celsius to celsius

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-celsius = float(input("Enter temperature in Celsius: "))
+celsius = float(input("Enter temperature in celsius: "))
 fahrenheit = celsius * 1.8 + 32.0
 print(str(celsius) + "° in Celsius is equivalent to " + str(fahrenheit) + "° Fahrenheit.")
